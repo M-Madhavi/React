24e4503528f7d5858ce38a650a613a21cf899b0f
Madhavi <Madhavinaidu424@gmail.com>
2026-02-20 16:07:38 +0530

    updated app.js

--- react-app-ignition/app.js
+++ react-app-ignition/app.js
@@ -2,6 +2,6 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 
 const heading = React.createElement("h1",{id:"heading"},"React is here")
-const root =createRoot(document.getElementById("root"))
+const root = createRoot(document.getElementById("root"))
 
 root.render(heading)
