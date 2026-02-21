dcb70128f000f23900830311c36d725cd19449be
Madhavi <Madhavinaidu424@gmail.com>
2026-02-21 22:38:00 +0530

    react-component

---
+++ react-food-app/utils/mockData.js
@@ -0,0 +1,44 @@
+const restaurantList = [
+  {
+    id: 1,
+    name: "Shah Ghouse Café",
+    cuisine: "Hyderabadi",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/56/Shah_Ghouse_Cafe_Hyderabad.jpg",
+    rating: 4.5,
+    time: "30 mins",
+  },
+  {
+    id: 2,
+    name: "Bawarchi Restaurant",
+    cuisine: "Indian, Asian",
+    image: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
+    rating: 4.3,
+    time: "25 mins",
+  },
+  {
+    id: 3,
+    name: "Kritunga – The Palegar Cuisine",
+    cuisine: "Andhra, South Indian",
+    image: "https://images.pexels.com/photos/7045694/pexels-photo-7045694.jpeg",
+    rating: 4.5,
+    time: "35 mins",
+  },
+  {
+    id: 4,
+    name: "Pakka Local",
+    cuisine: "South Indian, Biryani",
+    image: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
+    rating: 4.3,
+    time: "30 mins",
+  },
+  {
+    id: 5,
+    name: "California Burrito",
+    cuisine: "American, Mexican",
+    image: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
+    rating: 4.6,
+    time: "25 mins",
+  },
+];
+export default restaurantList;
