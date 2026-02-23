b3a90b1f3f638f422c81f8973c276663de5fd04c
Madhavi <Madhavinaidu424@gmail.com>
2026-02-23 21:47:34 +0530

    Added login button

--- react-food-app/src/components/Header/Header.js
+++ react-food-app/src/components/Header/Header.js
@@ -1,13 +1,12 @@
-import {LOGO_URL} from '../../../utils/constant'
-
+import { LOGO_URL } from "../../../utils/constant";
+import { useState } from "react";
 const HeaderComponent = () => {
+  const [btnNameReact, SetBtnNameReact] = useState("LogIn");
+  console.log("whole Header Component is Rerendered when clicked on button");
   return (
     <div className="header">
       <div>
-        <img
-          className="logo"
-          src={LOGO_URL}
-        />
+        <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-items">
         <ul>
@@ -15,10 +14,19 @@ const HeaderComponent = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "LogIn"?
+              SetBtnNameReact("LogOut"): SetBtnNameReact("LogIn");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
   );
 };
 
-export default HeaderComponent
+export default HeaderComponent;
