4aadbb2c77a43f8fabde490cb9f9aaabb8394563
Madhavi <Madhavinaidu424@gmail.com>
2026-02-28 20:30:03 +0530

    error page

---
+++ react-food-app/src/components/Error/Error.js
@@ -0,0 +1,15 @@
+import {useRouteError} from 'react-router'
+
+const Error =() =>{
+    const err = useRouteError();
+    console.log("err",err)
+    return (
+        <div>
+        <h1>Oops!!! something went wrong</h1>
+        <h3>{err.status}:{err.statusText}</h3>
+        </div>
+
+    )
+}
+
+export default Error
