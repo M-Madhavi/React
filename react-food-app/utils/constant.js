1881edc043f4d67bca3e5f0ac8238b58107c4013
Madhavi <madhavinaidu424@gmail.com>
2026-03-04 19:58:11 +0530

    update constant

--- react-food-app/utils/constant.js
+++ react-food-app/utils/constant.js
@@ -1,7 +1,8 @@
+
 export const LOGO_URL =
   "https://placeit-img-1-p.cdn.aws.placeit.net/uploads/stage/stage_image/68218/optimized_product_thumb_stage.jpg";
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const RESTAURANT_IMAGES = [
   "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg",
   "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
