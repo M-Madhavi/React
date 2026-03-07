7d682e21e85bcde056dc8862482279abe6c65503
Madhavi <madhavinaidu424@gmail.com>
2026-03-07 20:59:35 +0530

    class based component

--- react-food-app/src/components/UserComponent/UserClass.js
+++ react-food-app/src/components/UserComponent/UserClass.js
@@ -8,34 +8,47 @@ class UserClass extends React.Component {
         console.log(props)
         //creating a state variable in class based component->use- this.state.count
         this.state = {
-            count: 0,
-            count1: 1
+            userInfo:{
+                name:"",
+                location:"",
 
+            }
         }
     }
-    componentDidMount(){
+    async componentDidMount() {
         console.log("will be called as soon as the component is rendered");
+        const data = await fetch(process.env.GIT_URL)
+        const json = await data.json()
+        console.log("JSON",json);
+        this.setState({
+            userInfo:json
+        })
+
+
+    }
+
+    componentDidUpdate(){
+        console.log("component Updated");
         
     }
+
+    componentWillUnmount(){
+        console.log("componentUmount userclass");
+
+    }
     render() {
-        const {count} = this.state
+        const {name,location,avatar_url} = this.state.userInfo
         return (
             <div className="user-card">
                 <img
                     className="user-img"
-                    src="https://i.pravatar.cc/150?img=12"
+                    src={avatar_url}
                     alt="profile"
                 />
 
-                <h2>Name: MadhaVi</h2>
-                <h3>Location: Hyderabad</h3>
-                <h4>Contact: madhavim018@gmail.com</h4>
-                <button onClick={() => {
-                    this.setState({
-                        count: this.state.count+1
-                    })
-                }}>vote</button>
-                <h5>{count}</h5>
+                <h2>Name: {name}</h2>
+                <h3>Location: {location}</h3>
+                {/* <h4>Contact: </h4> */}
             </div>
         );
     }
