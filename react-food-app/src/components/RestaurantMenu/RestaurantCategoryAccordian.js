232d0d01aaa9fa29d4a817a85564e7d6f2e0b268
Madhavi <madhavinaidu424@gmail.com>
2026-03-16 22:12:45 +0530

    restaurant category accordian

--- react-food-app/src/components/RestaurantMenu/RestaurantCategoryAccordian.js
+++ react-food-app/src/components/RestaurantMenu/RestaurantCategoryAccordian.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
+import RestaurantItemList from "./RestaurantItemList";
+
 const RestaurantCategoryAccordian = ({ data }) => {
+    const [showItems, setShowItems] = useState(false)
+    const handleClick = () => {
+        console.log("clickeddddd")
+        setShowItems(!showItems)
+    }
 
     return (
-        <div className="flex justify-center" style={{ margin: '16px' }}>
-            <div className="w-6/12 bg-yellow-200 shadow-md rounded-lg px-6 py-4 flex justify-between items-center cursor-pointer hover:bg-gray-50 transition"style={{ padding: '18px' }}>
-                <span className="font-semibold text-lg">
-                    {data.title}
-                </span>
-                <span className="text-lg">
-                    ▼
-                </span>
-
+        <div className="flex justify-center" style={{ margin: '20px' }}>
+            <div className="w-6/12 bg-yellow-200 shadow-md rounded-lg px-6 py-4 items-center cursor-pointer hover:bg-gray-50 transition" style={{ padding: '18px' }}>
+                <div className="flex justify-between" onClick={handleClick}>
+                    <span className="font-semibold text-lg">
+                        {data.title} ({data.itemCards.length})
+                    </span>
+                    <span className="text-lg">
+                        {showItems ?"▲":"▼"}
+                    </span>
+                </div>
+                {/* {Accordion Body} */}
+                {showItems && <RestaurantItemList itemList={data.itemCards} />}
             </div>
+
         </div>
 
     );
