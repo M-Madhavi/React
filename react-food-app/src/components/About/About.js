071d89ef43c3de7fd644f60027586c0197acf308
Madhavi <madhavinaidu424@gmail.com>
2026-04-13 21:32:23 +0530

    usecontext

--- react-food-app/src/components/About/About.js
+++ react-food-app/src/components/About/About.js
@@ -1,3 +1,4 @@
+import UserContext from "../../../utils/UserContex";
 import UserClass from "../UserComponent/UserClass";
 import User from "../UserComponent/UserComponent";
 
@@ -5,6 +6,10 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <div>
+        LoggedIn User:
+        <UserContext.Consumer>{({ loggedInUser }) => <h5>{loggedInUser}</h5>}</UserContext.Consumer>
+      </div>
       <h2>This is About page</h2>
 
       <div className="user-container">
