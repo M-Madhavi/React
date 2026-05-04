585cdc3733458476844e00a225de251477101793
Madhavi <madhavinaidu424@gmail.com>
2026-05-04 17:58:24 +0530

    craete cart slice

---
+++ react-food-app/utils/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const cartSlice = createSlice({
+    name:'cart',
+    initialState:{
+        items:[]
+    },
+    reducers:{
+        
+    }
+  
+})
+
+export default cartSlice
