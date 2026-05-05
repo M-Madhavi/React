087d1497d1678c83bb2e61fe5556913c459217e1
Madhavi <madhavinaidu424@gmail.com>
2026-05-05 22:08:15 +0530

    craete cart slice

--- react-food-app/utils/cartSlice.js
+++ react-food-app/utils/cartSlice.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const cartSlice = createSlice({
-    name:'cart',
-    initialState:{
-        items:[]
+    name: 'cart',
+    initialState: {
+        items: []
     },
-    reducers:{
+    reducers: {
         //action
-        addItems: (state,action) =>{
+        addItems: (state, action) => {
             state.items.push(action.payload)
 
         },
-        removeItem:(state,action) =>{
-             state.items.pop()
+        removeItem: (state, action) => {
+            state.items.pop()
         },
-        clearCart:(state) =>{
+        clearCart: (state) => {
             state.items = [] //state.items.length = 0
         }
     }
-  
+
 })
-export const {addItems,removeItem,clearCart} = cartSlice.actions;
+export const { addItems, removeItem, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
